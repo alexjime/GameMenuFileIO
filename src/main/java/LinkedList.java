66a333ad2bfcb19a83e963896042e0c2dedf230a
alexjime <alexji.dev@gmail.com>
2020-06-05 20:26:12 +0900

    create remove, print

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -19,7 +19,35 @@ class LinkedList<U> {
         size++;
     }
 
+    public void remove(String removeId) {
+        for (Node<U> i = head.next; i != null; i = i.next) {
+            // Score의 getter를 사용하기 위해 제너릭타입 매개변수와 Score를 instanceof해서 명시
+            // i.data의 타입은 Score
+            if (i.data instanceof Score) {
+                String studentIds = ((Score) i.data).getStudentId();
+                if(removeId.equals(studentIds)) {
+                    i.data = i.next.data;
+                    i.next = i.next.next;
+                    break;
+                }
+            }
+        }
+        System.out.println("삭제가 완료되었습니다.");
+    }
+
     public int getSize() {
         return size;
+    }
+
+    public void print() {
+        System.out.println("==================");
+        for (Node<U> i = head.next; i != null; i = i.next) {
+            if (i.data instanceof Score) {
+                String id = ((Score) i.data).getStudentId();
+                int score = ((Score) i.data).getScore();
+                System.out.println(id + ", " + score);
+            }
+        }
+        System.out.println("==================");
     }
 }
